4d6ea56534b7b186829b7d7c331d03b0cd00ab2f
qgeffard <q.geffard@gmail.com>
2015-03-16 13:40:16 +0100

    add log4j

--- PlaylisytMetierHttpClient/src/main/java/org/kgj/pds/playlist/metier/httpClient/ClientHttpBoot.java
+++ PlaylisytMetierHttpClient/src/main/java/org/kgj/pds/playlist/metier/httpClient/ClientHttpBoot.java
@@ -1,27 +1,43 @@
 package org.kgj.pds.playlist.metier.httpClient;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.kgj.pds.playlist.metier.messagingService.ClientHttpMessagingServiceManager;
 
 /**
  * Class used to init all ClientHttp side
+ * 
  * @author Doox
  *
  */
-public class ClientHttpBoot 
-{
+public class ClientHttpBoot {
 	
-    public static void main( String[] args )
-    {
-        startMessagingService();
-        //...
-    }
-    
-    /**
-     * Demarre le service de messagerie grace a la methode getInstance pour creer le singleton
-     */
+	static Logger logger = Logger.getLogger(ClientHttpBoot.class);
+
+	public static void main(String[] args) {
+		startLoggerService();
+		startMessagingService();
+		// ...
+	}
+
+	/**
+	 * Demarre le service de logging
+	 */
+	private static void startLoggerService() {
+		BasicConfigurator.configure();
+		logger.debug("Hello World!");
+		logger.info("Info");
+		logger.warn("warning!");
+		logger.error("error");
+
+	}
+
+	/**
+	 * Demarre le service de messagerie grace a la methode getInstance pour
+	 * creer le singleton
+	 */
 	private static void startMessagingService() {
 		ClientHttpMessagingServiceManager.getInstance();
 	}
 
-
-	
 }
