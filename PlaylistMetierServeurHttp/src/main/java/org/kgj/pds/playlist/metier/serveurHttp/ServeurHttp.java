365a24d193be1304cdcf24ce0fcc2cbf3c45b634
qgeffard <q.geffard@gmail.com>
2015-03-17 15:02:51 +0100

    add webapp

---
+++ PlaylistMetierServeurHttp/src/main/java/org/kgj/pds/playlist/metier/serveurHttp/ServeurHttp.java
@@ -0,0 +1,53 @@
+package org.kgj.pds.playlist.metier.serveurHttp;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServeurHttp
+ */
+public class ServeurHttp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServeurHttp() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("request received");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ PlaylisytMetierHttpClient/src/main/java/org/kgj/pds/playlist/metier/ClientHttpBoot.java
@@ -0,0 +1,64 @@
+package org.kgj.pds.playlist.metier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.kgj.pds.playlist.metier.messagingService.ClientHttpMessagingServiceManager;
+
+/**
+ * Class used to init all ClientHttp side
+ * 
+ * @author Doox
+ *
+ */
+public class ClientHttpBoot {
+	
+	static Logger logger = Logger.getLogger(ClientHttpBoot.class);
+
+	public static void main(String[] args) {
+		startLoggerService();
+		startMessagingService();
+		// ...
+		
+		tryWSquery();
+	}
+
+	private static void tryWSquery() {
+		// TODO Auto-generated method stub
+		
+		try {
+			URLConnection connection = new URL("http://localhost:8080/PlaylistMetierServeurHttp/serveurHttp").openConnection();
+			connection.setRequestProperty("Accept-Charset", "utf-8");
+			InputStream response = connection.getInputStream();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * Demarre le service de logging
+	 */
+	private static void startLoggerService() {
+		BasicConfigurator.configure();
+	}
+
+	/**
+	 * Demarre le service de messagerie grace a la methode getInstance pour
+	 * creer le singleton
+	 */
+	private static void startMessagingService() {
+		ClientHttpMessagingServiceManager.getInstance();
+	}
+
+}
